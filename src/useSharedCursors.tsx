f1db3ea4d30c72125b577a1f17fe112940153bc0
Ian Macartney <ianhmacartney@gmail.com>
2024-01-13 00:22:57 -0800

    take out unecessary sessionId reference

--- src/useSharedCursors.tsx
+++ src/useSharedCursors.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { api } from "../convex/_generated/api";
 import { Doc, Id } from "../convex/_generated/dataModel";
-import { useSessionId, useSessionMutation } from "./useServerSession";
+import { useSessionMutation } from "./useServerSession";
 import { useQuery } from "convex/react";
 import { Knobs, makePositionHooks } from "./usePositionHooks";
 
@@ -44,10 +44,6 @@ export function useSharedCursors(
     throw new Error(
       "useSharedCursor must be used within a SharedCursorProvider"
     );
-  }
-  const sessionId = useSessionId();
-  if (sessionId === undefined) {
-    throw new Error("useSharedCursor must be used within a SessionProvider");
   }
   const { zone, cursor } = context;
   const cursors = useQuery(api.lib.cursors.listCursors, { zone });
